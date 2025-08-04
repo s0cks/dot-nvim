3c6ac9b08f04da30941ec1d8f77bd025f4da62bd
s0cks <tasman.vose@gmail.com>
2025-08-04 15:58:38 -0400

    fix(precognition,neo-tree): :bug: fix configurations

--- lua/ui/precognition/init.lua
+++ lua/ui/precognition/init.lua
@@ -3,16 +3,12 @@ return {
     'tris203/precognition.nvim',
     event = 'VeryLazy',
     opts = {
+      startVisible = false,
       showBlankVirtLine = false,
       highlightColor = { link = 'Comment' },
       disabled_fts = {
         'dashboard',
       },
     },
-    config = function(_, opts)
-      local precognition = require('precognition')
-      precognition.config(opts or {})
-      precognition.hide()
-    end,
   },
 }
