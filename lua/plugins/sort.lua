cd0eeaa62c6d403cfb630555088021ebc587d7c6
tazz <tazz@tazz.cloud>
2025-12-11 11:27:04 -0500

    refactor(plugins/sort): :recycle: add default opts for sort plugin

--- lua/plugins/sort.lua
+++ lua/plugins/sort.lua
@@ -1,3 +1,38 @@
 return {
   'sQVe/sort.nvim',
+  opts = {
+    -- Delimiter priority order.
+    delimiters = {
+      ',',
+      '|',
+      ';',
+      ':',
+      's', -- Space.
+      't', -- Tab.
+    },
+
+    -- Natural sorting (default: true).
+    natural_sort = true,
+
+    -- Case-insensitive sorting (default: false).
+    ignore_case = false,
+
+    -- Whitespace alignment threshold.
+    whitespace = {
+      alignment_threshold = 3,
+    },
+
+    -- Default keymappings (set to false to disable).
+    mappings = {
+      operator = 'go',
+      textobject = {
+        inner = 'io',
+        around = 'ao',
+      },
+      motion = {
+        next_delimiter = ']o',
+        prev_delimiter = '[o',
+      },
+    },
+  },
 }
