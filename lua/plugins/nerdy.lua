5fb9647cc231de80cb3be0381941b6982a902bd4
tazz <tazz@tazz.cloud>
2025-12-11 11:27:05 -0500

    feat(plugins): :sparkles: update nerdy plugin to lazy load on command

--- lua/plugins/nerdy.lua
+++ lua/plugins/nerdy.lua
@@ -1,12 +1,9 @@
---- Nerdy - NerdFont picker using snacks.nvim picker
----@url https://github.com/2kabhishek/nerdy.nvim
 ---@type LazyPluginSpec
 return {
   '2kabhishek/nerdy.nvim',
   dependencies = {
     'folke/snacks.nvim',
   },
-  event = 'VeryLazy',
   cmd = 'Nerdy',
   opts = {},
 }
