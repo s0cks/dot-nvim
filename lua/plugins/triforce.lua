6154565cd32a0b4d9261cdeb494283b5b6ecd912
tazz <tazz@tazz.cloud>
2025-12-11 11:27:02 -0500

    feat(plugins/triforce): :sparkles: add gisketch/triforce.nvim plugin
    add [gisketch/triforce.nvim](https://github.com/gisketch/triforce.nvim) plugin

---
+++ lua/plugins/triforce.lua
@@ -0,0 +1,14 @@
+---Triforce - Neovim gamification
+---@url https://github.com/gisketch/triforce.nvim
+---@type LazyPluginSpec
+return {
+  'gisketch/triforce.nvim',
+  dependencies = {
+    'nvzone/volt',
+  },
+  opts = {
+    keymap = {
+      show_profile = '<leader>tri', -- Open profile with <leader>tp
+    },
+  },
+}
