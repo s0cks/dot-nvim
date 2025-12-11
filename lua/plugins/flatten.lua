9e39a7ef050d9f4c1eb1a0ae878ec1ce1e09f257
tazz <tazz@tazz.cloud>
2025-12-11 11:27:08 -0500

    feat(plugins): :sparkles: add flatten plugin
    add [willothy/flatten.nvim](https://github.com/willothy/flatten.nvim)

---
+++ lua/plugins/flatten.lua
@@ -0,0 +1,15 @@
+return {
+  'willothy/flatten.nvim',
+  lazy = false,
+  priority = 1001,
+  opts = {
+    integrations = {
+      kitty = true,
+    },
+  },
+  window = {
+    open = 'smart',
+    diff = 'tab_vsplit',
+    focus = 'first',
+  },
+}
