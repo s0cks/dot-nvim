09ef2be0a5f99a0b9f663c328912bf71cdb28857
tazz <tazz@tazz.cloud>
2025-12-11 11:27:06 -0500

    feat(plugins): :sparkles: remove Gittory plugin
    remove Gittory plugin in favour of .editorconfig files

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -28,22 +28,6 @@ return {
     },
   },
   {
-    ---@url https://github.com/IsWladi/Gittory
-    ---@type LazyPluginSpec
-    'IsWladi/Gittory',
-    version = '*',
-    dependencies = {
-      'j-hui/fidget.nvim',
-    },
-    opts = {
-      atStartUp = true,
-      notifySettings = {
-        enabled = true,
-        availableNotifyPlugins = { 'fidget', 'print' },
-      },
-    },
-  },
-  {
     ---@url https://github.com/pwntester/octo.nvim
     ---@type LazyPluginSpec
     'pwntester/octo.nvim',
