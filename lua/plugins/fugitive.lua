a2f18924ca6df2593d18de205628b6bb88b0d147
tazz <tazz@tazz.cloud>
2025-12-11 11:27:14 -0500

    feat(plugins/fugitive): :sparkles: add mapping for running git push
    add mapping ,Gp for running git push command

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,15 @@
----@url https://github.com/tpope/vim-fugitive
 ---@type LazyPluginSpec
 return {
   'tpope/vim-fugitive',
   event = 'VeryLazy',
   opts = {},
   config = function() end,
+  keys = {
+    {
+      '<leader>Gp',
+      ':Git push',
+      'n',
+      desc = 'Git push',
+    },
+  },
 }
