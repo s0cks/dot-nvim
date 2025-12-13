2d69dafb967479430cfc2e6cc16ea9976a93d93e
tazz <tazz@tazz.cloud>
2025-12-13 02:54:35 -0500

    feat(plugins/neogen): :sparkles: add some keymaps for neogen
    Closes #17

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,7 +1,50 @@
----@type LazyPluginSpec
+---@author tazz
+---@license MIT
 return {
   'danymat/neogen',
   version = '*',
   cmd = 'Neogen',
   opts = {},
+  keys = {
+    {
+      '<leader>ngf',
+      function()
+        require('neogen').generate({
+          type = 'func',
+        })
+      end,
+      'n',
+      desc = 'Generate func docs using Neogen',
+    },
+    {
+      '<leader>ngF',
+      function()
+        require('neogen').generate({
+          type = 'file',
+        })
+      end,
+      'n',
+      desc = 'Generate file docs using Neogen',
+    },
+    {
+      '<leader>ngc',
+      function()
+        require('neogen').generate({
+          type = 'class',
+        })
+      end,
+      'm',
+      desc = 'Generate class docs using Neogen',
+    },
+    {
+      '<leader>ngt',
+      function()
+        require('neogen').generate({
+          type = 'type',
+        })
+      end,
+      'n',
+      desc = 'Generate type docs using Neogen',
+    },
+  },
 }
