a370c74db2a3fe2b2e2dd11ed12e6cabe6de21cb
tazz <tazz@tazz.cloud>
2025-12-16 09:42:28 -0500

    feat(plugins/pomodoro): ✨ make heirline component update every other second

--- lua/plugins/heirline/components/pomodoro.lua
+++ lua/plugins/heirline/components/pomodoro.lua
@@ -2,13 +2,9 @@ return {
   init = function(self)
     ---TODO(@s0cks): implement
   end,
-  update = {
-    'User',
-    pattern = 'GitSignsUpdate',
-    callback = vim.schedule_wrap(function()
-      vim.cmd([[redrawstatus]])
-    end),
-  },
+  update = function()
+    return vim.fn.localtime() % 2 == 0
+  end,
   provider = function()
     return require('pomodoro').get_pomodoro_status('no tomato', 'tomato', 'break')
   end,
