009b7d4e8aac468677623b8f3dea7b67932e5111
tazz <tazz@tazz.cloud>
2025-12-23 20:07:23 -0500

    fix(plugins/modes): 🐛 fix colours being applied to modes plugin?

--- lua/plugins/modes.lua
+++ lua/plugins/modes.lua
@@ -1,12 +1,3 @@
-local hl_palette = {
-  ModesCopy = 'ye',
-  ModesDelete = 're',
-  ModesFormat = 'or',
-  ModesInsert = 'cy',
-  ModesVisual = 'pu',
-  ModesReplace = 'bl',
-}
-
 return {
   'mvllow/modes.nvim',
   version = '*',
@@ -14,38 +5,40 @@ return {
   dependencies = {
     'kepano/flexoki-neovim',
   },
-  opts = {
-    -- Set opacity for cursorline and number background
-    line_opacity = 0.25,
+  opts = function()
+    local colors = require('theme.colors')
+    return {
+      colors = {
+        bg = '', -- Optional bg param, defaults to Normal hl group
+        copy = '#f5c359',
+        delete = '#c75c6a',
+        change = '#c75c6a', -- Optional param, defaults to delete
+        format = '#c79585',
+        insert = '#78ccc5',
+        replace = '#245361',
+        select = colors['pu'], -- Optional param, defaults to visual
+        visual = colors['pu'],
+      },
+      -- Set opacity for cursorline and number background
+      line_opacity = 0.65,
 
-    -- Enable cursor highlights
-    set_cursor = true,
+      -- Enable cursor highlights
+      set_cursor = true,
 
-    -- Enable cursorline initially, and disable cursorline for inactive windows
-    -- or ignored filetypes
-    set_cursorline = true,
+      -- Enable cursorline initially, and disable cursorline for inactive windows
+      -- or ignored filetypes
+      set_cursorline = true,
 
-    -- Enable line number highlights to match cursorline
-    set_number = true,
+      -- Enable line number highlights to match cursorline
+      set_number = true,
 
-    -- Enable sign column highlights to match cursorline
-    set_signcolumn = true,
+      -- Enable sign column highlights to match cursorline
+      set_signcolumn = true,
 
-    -- Disable modes highlights for specified filetypes
-    -- or enable with prefix "!" if otherwise disabled (please PR common patterns)
-    -- Can also be a function fun():boolean that disables modes highlights when true
-    ignore = { 'NvimTree', 'TelescopePrompt', '!minifiles' },
-  },
-  init = function()
-    local c = require('flexoki.palette').palette()
-    local set_hl_bg = function(group, color)
-      vim.api.nvim_set_hl(0, group, {
-        bg = c[color] and c[color] or color,
-      })
-    end
-
-    for group, color in pairs(hl_palette) do
-      set_hl_bg(group, color)
-    end
+      -- Disable modes highlights for specified filetypes
+      -- or enable with prefix "!" if otherwise disabled (please PR common patterns)
+      -- Can also be a function fun():boolean that disables modes highlights when true
+      ignore = { 'NvimTree', 'TelescopePrompt', '!minifiles' },
+    }
   end,
 }
