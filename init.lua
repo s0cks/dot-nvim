9969d480eae92fae63910882ae1fb3b8155ba78b
tazz <tasman.vose@gmail.com>
2026-05-03 21:11:36 -0400

    refactor: 🔨 working on refactoring

--- init.lua
+++ init.lua
@@ -1,24 +1,30 @@
 ---@author tazz
 ---@license MIT
 
-require('strings')
 require('settings')
-require('mappings')
+
+-- require('strings')
+-- require('mappings')
 
 local utils = require('utils')
 -- Bootstrap lazy.vim
-utils.clone_and_add_to_rtp('folke/lazy.nvim', vim.fn.stdpath('data') .. '/lazy/lazy.nvim')
+-- utils.clone_and_add_to_rtp('folke/lazy.nvim', vim.fn.stdpath('data') .. '/lazy/lazy.nvim')
+--
+-- utils.create_autocmds(require('autocmds'))
 
-utils.create_autocmds(require('autocmds'))
+-- require('lazy').setup({
+--   spec = {
+--     require('theme'),
+--     { import = 'plugins' },
+--   },
+--   install = {
+--     colorscheme = { 'flexoki-dark' },
+--     missing = true,
+--   },
+--   checker = { enabled = false, notify = false },
+-- })
 
-require('lazy').setup({
-  spec = {
-    require('theme'),
-    { import = 'plugins' },
-  },
-  install = {
-    colorscheme = { 'flexoki-dark' },
-    missing = true,
-  },
-  checker = { enabled = false, notify = false },
+vim.pack.add({
+  "https://github.com/s0cks/flexoki.nvim",
 })
+vim.cmd([[colorscheme flexoki-dark]])
